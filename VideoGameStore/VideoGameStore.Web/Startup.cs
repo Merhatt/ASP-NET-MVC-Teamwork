d73910df1b63d28e038a3ab0e7c2e3f189eca387
Merhatt <merhat1998@gmail.com>
2017-03-16 21:01:35 +0200

    Added skeleton

---
+++ VideoGameStore/VideoGameStore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoGameStore.Web.Startup))]
+namespace VideoGameStore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
